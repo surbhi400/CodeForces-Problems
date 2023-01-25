817846b32325d8a3d5a41c018eaca11103314349
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2023-01-25 16:43:02 +0530

    Problem-28(25-01-23)

---
+++ Ladder A/Elephant.cpp
@@ -0,0 +1,34 @@
+/* An elephant decided to visit his friend. It turned out that the elephant's house is located at point 0 and his friend's house is located at point x(x > 0) of the coordinate line. In one step the elephant can move 1, 2, 3, 4 or 5 positions forward. Determine, what is the minimum number of steps he need to make in order to get to his friend's house.
+
+Input
+The first line of the input contains an integer x (1 ≤ x ≤ 1 000 000) — The coordinate of the friend's house.
+
+Output
+Print the minimum number of steps that elephant needs to make to get from point 0 to point x.
+
+Examples
+inputCopy
+5
+outputCopy
+1
+inputCopy
+12
+outputCopy
+3 */
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    int ans = 0;
+    ans = x / 5;
+    if (x % 5 != 0)
+    {
+        ans += 1;
+    }
+    cout << ans << endl;
+    return 0;
+}
