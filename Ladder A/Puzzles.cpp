bb90deac6775162b4c60f51d22b02acadf68bcea
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2023-02-06 22:33:50 +0530

    Problem-40 (05-02-23)

--- Ladder A/Puzzles.cpp
+++ Ladder A/Puzzles.cpp
@@ -2,8 +2,8 @@
 her n students and give each of them a jigsaw puzzle (which, as wikipedia states, is a tiling puzzle that requires the assembly of numerous small, often oddly shaped,
 interlocking and tessellating pieces).
 
-The shop assistant told the teacher that there are m puzzles in the shop, but they might differ in difficulty and size. Specifically, the first jigsaw puzzle consists of
-f1 pieces, the second one consists of f2 pieces and so on.
+The shop assistant told the teacher that there are m puzzles in the shop, but they might differ in difficulty and size. Specifically, the first jigsaw puzzle consists 
+of f1 pieces, the second one consists of f2 pieces and so on.
 
 Ms. Manana doesn't want to upset the children, so she decided that the difference between the numbers of pieces in her presents must be as small as possible. Let A be
 the number of pieces in the largest puzzle that the teacher buys and B be the number of pieces in the smallest such puzzle. She wants to choose such n puzzles that
