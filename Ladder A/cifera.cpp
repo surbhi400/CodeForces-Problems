2a178d541b09e073730f105a9d7c37f8b300ed09
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2023-03-07 19:54:35 +0530

    07-03-23 (Problem-57)

---
+++ Ladder A/cifera.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int k,l,importance(-1);
+    cin>>k>>l;
+    while(l%k==0){
+        l/=k;
+        importance+=1;
+    }
+    if(importance >=0 && l==1){
+        cout<<"YES" <<endl<<importance<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
