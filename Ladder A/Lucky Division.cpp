9e4c8095a3553d79814250644297811fe8a39941
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2023-03-09 19:55:10 +0530

    09-03-23 (Problem-59)

---
+++ Ladder A/Lucky Division.cpp
@@ -0,0 +1,45 @@
+/* Petya loves lucky numbers. Everybody knows that lucky numbers are positive integers whose decimal representation contains only the lucky digits 4 and 7. For example,
+numbers 47, 744, 4 are lucky and 5, 17, 467 are not.
+
+Petya calls a number almost lucky if it could be evenly divided by some lucky number. Help him find out if the given number n is almost lucky.
+
+Input
+The single line contains an integer n (1 ≤ n ≤ 1000) — the number that needs to be checked.
+
+Output
+In the only line print "YES" (without the quotes), if number n is almost lucky. Otherwise, print "NO" (without the quotes).
+
+Examples
+inputCopy
+47
+outputCopy
+YES
+inputCopy
+16
+outputCopy
+YES
+inputCopy
+78
+outputCopy
+NO
+Note
+Note that all lucky numbers are almost lucky as any number is evenly divisible by itself.
+
+In the first sample 47 is a lucky number. In the second sample 16 is divisible by 4. */
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n%4==0 || n%7==0 || n%44==0|| n%47==0 || n%74==0 ||n%77==0 || n%444==0 || n%447==0 || n%474==0 || n%744==0 || n%477==0 || n%747==0 || n%774==0 || n%777==0 )
+    {
+        cout<<"YES";
+    }
+    else
+        {
+        cout<<"NO";
+    }
+    return 0;
+}
